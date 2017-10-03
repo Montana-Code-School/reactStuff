f2e89faf2dffcd3f76a7aff0ee27ac935f06507f
kramduckner <buckner.a.mark@gmail.com>
2017-10-02 19:07:55 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Barf extends React.Component{
+  render(){
+    return  (
+      <div>
+        hello my name is fart head
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(<Barf />, document.getElementById('root'));

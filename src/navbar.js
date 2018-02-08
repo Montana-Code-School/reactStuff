1781933468ce6c29100cafa6c91bbf0ec57133c6
kramduckner <buckner.a.mark@gmail.com>
2018-02-08 11:48:24 -0700

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -1,2 +1,2 @@
-//find and edit the index.html file to include bootstrap
-//add a navbar to the application, but do it within a react component
+//Add react strap to your application. https://reactstrap.github.io/ there are great guides to do it on the site.
+//Using reactstrap, add a navbar to the application, but do it within a react component

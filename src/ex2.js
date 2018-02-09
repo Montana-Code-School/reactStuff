25585c81440933220a1993acb9abaa48ff30e817
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 12:31:52 -0700

    Update ex2.js

--- src/ex2.js
+++ src/ex2.js
@@ -1,3 +1,3 @@
 //this is an easy one. Components as an idea can be reused - that's 
 //integral to their purpose. Take one of the components you've used
-//and use several of them in your application.
+//and use it several times in your application.

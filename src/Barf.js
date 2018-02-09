3a726e1fa8538414a616cea9548592f209077bcb
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 12:27:38 -0700

    Update Barf.js

--- src/Barf.js
+++ src/Barf.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 //Create another react component the same way as you did before
 //This time, we're going to use this component WITHIN your other 
-//component - that's right, we can nest components. Call the component
+//component - that's right, we can nest components. In fact, you'll only 
+//attach one component to an html tag - that is the entry point of your react. 
+//ideally, you won't be doing anymore edits in html. Back to the component - Call it
 // "Barf"
 
 //export it in this file, then import it into your index.js file and put the tag
-//inside the div's in your render method and viola! 
+//inside the div's in your render method and viola! Look at your hard work in the browser

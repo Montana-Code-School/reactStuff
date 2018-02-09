97da710aa4faf368a735d834be022e27805a0d85
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 12:32:25 -0700

    Update props.js

--- src/props.js
+++ src/props.js
@@ -1,3 +1,3 @@
-//create or use two react components you have built. next one inside the other. 
+//create or use two react components you have built. nest one inside the other. 
 //In the top most component, pass a 'prop' into it and display that 'prop'
 //in the bottom component. You'll probably need to look this up

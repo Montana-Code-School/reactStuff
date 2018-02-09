5357ecabfbab16fcf48a547592812e82b12b6db2
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 12:29:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/lib/Button';
 
-//Let's use some variables IN THE TEMPLATE WOW OMG GEEZLAWEEZ
+//Let's use some variables IN THE TEMPLATE WOW OMG LOWL
 //in the app component below, create a constructor function (don't 
 //forget to call super() since it's a subclass) that declares a variable 
 //spaceDawgz. Set it to some wicked cool value. Then, inject
-//this component into the application and notice what happens
+//this component into the application (like we did with Barf) and notice what happens
 //in the app! You can pass in variables into the template the application!
-
+//Inject another variable into this component copying the syntax. There's not really 
+//an elegant way of doing so with HTML. This is one of the benefits of React.
 
 class App extends Component {
 

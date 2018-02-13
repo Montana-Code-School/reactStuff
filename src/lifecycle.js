2589a25709d902cc4b289d54b76f6635b8b8ff27
kramduckner <buckner.a.mark@gmail.com>
2018-02-12 19:37:16 -0700

    Update lifecycle.js

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+//make a button and maybe have them click it... ordering
+//order both the state and the render methods. have a button change state..
 
 export default class App extends Component {
 

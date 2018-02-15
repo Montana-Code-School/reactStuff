8cce4355837ad28df85a6eaa1c92c8c93a076747
kramduckner <buckner.a.mark@gmail.com>
2018-02-15 14:12:46 -0700

    Update ex3.js

--- src/ex3.js
+++ src/ex3.js
@@ -1,5 +1,6 @@
 //using react components, create a 3 x 3 table,  where each cell in the table
 //contains a react component that displays the integers 1 - 9 like this:
+//You'll create an array in a parent component and pass each value in the array into children components
 //1 2 3
 //3 4 5
 //6 7 8
